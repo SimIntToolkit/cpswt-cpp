dc354e64fbde2d83dc3cb6a00982f14ba534d578
Harmon Nine <Harmon.Nine@gmail.com>
2022-01-14 16:19:25 -0600

    Add copyright/license/disclaimer to generated code

--- foundation/rti-base/src/main/include/InteractionRoot_p/C2WInteractionRoot_p/SimLog.hpp
+++ foundation/rti-base/src/main/include/InteractionRoot_p/C2WInteractionRoot_p/SimLog.hpp
@@ -1,25 +1,38 @@
+
 /*
- * Copyright (c) 2008, Institute for Software Integrated Systems,
- * Vanderbilt University
- * All rights reserved.
+ * Certain portions of this software are Copyright (C) 2006-present
+ * Vanderbilt University, Institute for Software Integrated Systems.
  *
- * Permission to use, copy, modify, and distribute this software and its
- * documentation for any purpose, without fee, and without written agreement is
- * hereby granted, provided that the above copyright notice, the following
- * two paragraphs and the author appear in all copies of this software.
+ * Certain portions of this software are contributed as a public service by
+ * The National Institute of Standards and Technology (NIST) and are not
+ * subject to U.S. Copyright.
  *
- * IN NO EVENT SHALL THE VANDERBILT UNIVERSITY BE LIABLE TO ANY PARTY FOR
- * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
- * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE VANDERBILT
- * UNIVERSITY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
- * THE VANDERBILT UNIVERSITY SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
- * ON AN "AS IS" BASIS, AND THE VANDERBILT UNIVERSITY HAS NO OBLIGATION TO
- * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ * The above Vanderbilt University copyright notice, NIST contribution
+ * notice and this permission and disclaimer notice shall be included in all
+ * copies or substantial portions of the Software.
  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE. THE AUTHORS OR COPYRIGHT HOLDERS SHALL NOT HAVE
+ * ANY OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
+ * OR MODIFICATIONS.
+ *
+ * @author Himanshu Neema
+ * @author Harmon Nine
  */
+
+
 
 #ifndef _org_cpswt_hla_InteractionRoot_p_C2WInteractionRoot_p_SimLog_CLASS
 #define _org_cpswt_hla_InteractionRoot_p_C2WInteractionRoot_p_SimLog_CLASS
@@ -424,28 +437,6 @@ public:
 
 
     /**
-     * Set the value of the "FedName" parameter to "value" for this parameter.
-     *
-     * @param value the new value for the "FedName" parameter
-     */
-    void set_FedName(const std::string & newValue) {
-        ClassAndPropertyName key(get_hla_class_name(), "FedName");
-        Value &value(*_classAndPropertyNameValueSPMap[key]);
-        value.setValue(newValue);
-    }
-
-    /**
-     * Returns the value of the "FedName" parameter of this interaction.
-     *
-     * @return the value of the "FedName" parameter
-     */
-    std::string get_FedName() {
-        ClassAndPropertyName key(get_hla_class_name(), "FedName");
-        return static_cast<std::string>(*_classAndPropertyNameValueSPMap[key]);
-    }
-
-
-    /**
      * Set the value of the "Comment" parameter to "value" for this parameter.
      *
      * @param value the new value for the "Comment" parameter
@@ -463,6 +454,28 @@ public:
      */
     std::string get_Comment() {
         ClassAndPropertyName key(get_hla_class_name(), "Comment");
+        return static_cast<std::string>(*_classAndPropertyNameValueSPMap[key]);
+    }
+
+
+    /**
+     * Set the value of the "FedName" parameter to "value" for this parameter.
+     *
+     * @param value the new value for the "FedName" parameter
+     */
+    void set_FedName(const std::string & newValue) {
+        ClassAndPropertyName key(get_hla_class_name(), "FedName");
+        Value &value(*_classAndPropertyNameValueSPMap[key]);
+        value.setValue(newValue);
+    }
+
+    /**
+     * Returns the value of the "FedName" parameter of this interaction.
+     *
+     * @return the value of the "FedName" parameter
+     */
+    std::string get_FedName() {
+        ClassAndPropertyName key(get_hla_class_name(), "FedName");
         return static_cast<std::string>(*_classAndPropertyNameValueSPMap[key]);
     }
 
@@ -496,12 +509,12 @@ protected:
     void classAndPropertyNameValueSPMapInit() {
 
         _classAndPropertyNameValueSPMap.emplace( std::make_pair(
-          ClassAndPropertyName(get_hla_class_name(), "FedName"),
+          ClassAndPropertyName(get_hla_class_name(), "Comment"),
           ValueSP(  new Value( std::string("") )  )
         ) );
 
         _classAndPropertyNameValueSPMap.emplace( std::make_pair(
-          ClassAndPropertyName(get_hla_class_name(), "Comment"),
+          ClassAndPropertyName(get_hla_class_name(), "FedName"),
           ValueSP(  new Value( std::string("") )  )
         ) );
 
