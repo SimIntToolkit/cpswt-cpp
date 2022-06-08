12cac09399e61a2acfaf99d3226f2bc1c2f3c253
Harmon Nine <Harmon.Nine@gmail.com>
2022-06-08 18:43:29 -0500

    Test created, along with necessary modifications to the CppTestHarness

---
+++ foundation/CppTestHarness/src/main/include/GenericHandleValuePairForTesting.hpp
@@ -0,0 +1,63 @@
+/*
+ *   Copyright 2007 The Portico Project
+ *
+ *   This file is part of portico.
+ *
+ *   portico is free software; you can redistribute it and/or modify
+ *   it under the terms of the Common Developer and Distribution License (CDDL)
+ *   as published by Sun Microsystems. For more information see the LICENSE file.
+ *
+ *   Use of this software is strictly AT YOUR OWN RISK!!!
+ *   If something bad happens you do not have permission to come crying to me.
+ *   (that goes for your lawyer as well)
+ *
+ */
+#ifndef GENERIC_HANDLE_VALUE_PAIR_HPP
+#define GENERIC_HANDLE_VALUE_PAIR_HPP
+
+#include "RTIForTesting.hpp"
+
+class GenericHandleValuePair
+{
+	//----------------------------------------------------------
+	//                    STATIC VARIABLES
+	//----------------------------------------------------------
+
+	//----------------------------------------------------------
+	//                   INSTANCE VARIABLES
+	//----------------------------------------------------------
+	private:
+		RTI::Handle theHandle;
+		char *buffer;
+		RTI::ULong valueLength;
+
+	//----------------------------------------------------------
+	//                      CONSTRUCTORS
+	//----------------------------------------------------------
+	public:
+		GenericHandleValuePair();
+		~GenericHandleValuePair();
+
+	//----------------------------------------------------------
+	//                    INSTANCE METHODS
+	//----------------------------------------------------------
+	public:
+		RTI::Handle getHandle();
+		void setHandle( RTI::Handle newHandle );
+		void setValue( const char *newData, RTI::ULong newDataLength );
+		char* getValue();
+		RTI::ULong getValueLength();
+		
+		// sets the value to the given data, but DOESN'T COPY IT. this means that
+		// when call this method, it is passing responsibility for deleting it over
+		// to us and making the guarantee that the caller won't delete the data.
+		void setValueButDontCopy( char *newData, RTI::ULong newDataLength );
+
+	//----------------------------------------------------------
+	//                     STATIC METHODS
+	//----------------------------------------------------------
+
+};
+
+#endif
+
